bf1c0541d3b269a9cac1e5c6102d0b2a24f100f0
sv2421 <sv2421@columbia.edu>
2015-08-17 18:32:51 -0500

    Adding the kida_controller, a sample usage script, and a kida.uva.xxx file

---
+++ kida_controller.py
@@ -0,0 +1,110 @@
+class reaction:
+    def __init__(self, data, index):
+        """Initializes a reaction using all the data from a given line of the
+        kida.uva.xxxx file and that line's number."""
+        self.data = data
+        components = self.data.split()
+        self.index = index
+        self.reactants = components[0:2]
+        self.products = components[2:len(components) - 13]
+        self.rate_constant_coefficients = components[-13:-10]
+        self.uncertainty_coefficients = components[-10:-8]
+        self.uncertainty_type = components[-8]
+        self.type_of_reaction = components[-7]
+        self.valid_temperature_range = components[-6:-4]
+        self.formula = components[-4]
+        self.number = components[-3]
+        self.number_of_same_reaction = components[-2]
+        self.recommendation = components[-1]
+    
+    def to_kida_format(self):
+        """Formats all the data for a reaction in the standard KIDA format.
+        Returns a string containing all the formatted data for the reaction."""
+        kida = self.data.rsplit("               ", 1)
+        
+        coeffs = self.rate_constant_coefficients[0]
+        if float(self.rate_constant_coefficients[1]) < 0:
+            coeffs += (" " + self.rate_constant_coefficients[1])
+        else:
+            coeffs += ("  " + self.rate_constant_coefficients[1])
+            
+        if float(self.rate_constant_coefficients[2]) < 0:
+            coeffs += (" " + self.rate_constant_coefficients[2])
+        else:
+            coeffs += ("  " + self.rate_constant_coefficients[2])
+        
+        form = str(self.formula)
+        if(len(form) == 1):
+            string = kida[0] + "               " + coeffs + kida[1][len(coeffs):73] + form + kida[1][74:]
+        elif(len(form) == 2):
+            string = kida[0] + "               " + coeffs + kida[1][len(coeffs):72] + form + kida[1][74:]
+        return string
+            
+    def visualize(self):
+        """Provides a quick way to visualize a reaction as:
+        reactant1 + reactant2 ----> product1 + product2 + ..."""
+        visual = ""
+        for i, reactant in enumerate(self.reactants):
+            if i == len(self.reactants) - 1:
+                visual += reactant + " "
+            else:
+                visual += (reactant + " + ")
+        visual += "----> "
+        for i, product in enumerate(self.products):
+            if i == len(self.products) - 1:
+                visual += product
+            else:
+                visual += (product + " + ")
+        print visual
+
+def initialize_network(filename):
+    """This function takes in a kida.uva.xxxx file and creates an array of
+    reaction objects. This array is quickly iterable, allowing for faster 
+    traversal of the network than currently exiss on the KIDA webpage.""" 
+    network = []
+    network_file = open(filename)
+    for i, line in enumerate(network_file):
+        network.append(reaction(line, i))
+    return network
+
+def is_in_KIDA(reactants, products, network):
+    """This function takes in an array or tuple of reactants and products and
+    a network (an array of reaction objects). It checks to see if a reaction
+    with those reactants and products exists within the network. It returns
+    (True, reaction) if the reaction is found in the network, and (False, -1)
+    if not."""
+    for reaction in network:
+        if sorted(reaction.reactants) == sorted(reactants):
+            if sorted(reaction.products) == sorted(products):
+                return (True, network[reaction.index])
+    return (False, -1)
+
+def fortran_format(new_vals):
+    """This function takes an array of values and returns those values
+    formatted for use in the kida.uva.xxxx file (such that Nahoon
+    doesn't complain about the formatting)."""
+    return_vals = []
+    for val in new_vals:
+        return_vals.append(str('%.3E' % (float(val))).replace('E', 'e'))
+    return return_vals
+
+def change_val(reaction, network, new_rate_coef_constants):
+    """This function takes a reaction, the network in which that reaction
+    appears, and an array of rate coefficients constants. It changes the
+    reaction in the network to use the new rate coefficient constants."""
+    network[reaction.index].rate_constant_coefficients = fortran_format(new_rate_coef_constants)
+    
+def change_form(reaction, network, new_formula):
+    """This function takes a reaction, the network in which that reaction
+    appears, and a new formula number to calculate the rate coefficient.
+    It changes the reaction in the network to use the new formula number."""
+    network[reaction.index].formula = str(new_formula)
+
+def write_new_vals(network, filename):
+    """This takes a network of reactions and a filename to write those reactions
+    to. It formats those reactions in a KIDA-appropriate fashion and writes
+    them to the file specified."""
+    g = open(filename, 'w')
+    for val in network:
+        g.write(val.to_kida_format())
+    g.close()
---
+++ kida_controller_script.py
@@ -0,0 +1,72 @@
+from kida_controller import *
+
+#f = 'kida.uva.2014'
+####################GETTING THE KIDA FILE###########
+good_file = False
+while good_file == False:
+    try:
+        f = raw_input("Specify the initial condition file (CTRL+C to quit): ")
+        old_vals = initialize_network(f)
+        good_file = True
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        good_file = False
+
+if good_file == True: 
+    good_yes_no = False
+    while good_yes_no == False:
+        ################CHECKING IF THE FILE MUST BE CHANGED#####################
+        to_continue =  raw_input("Do you want to leave the " + f + " network unchanged? (YES or NO): ")   
+        if to_continue == "YES" or to_continue == "yes" or to_continue == "Y" or to_continue == "y":
+            print "You're good to go!"
+            good_yes_no = True
+        elif to_continue == "NO" or to_continue == "no" or to_continue == "N" or to_continue == "n":
+            good_to_go = False
+            while good_to_go == False:
+                how_many_input = False
+                while how_many_input == False:
+                    try:
+                        ##########CHECKING HOW MANY REACTIONS TO CHANGE##############
+                        how_many = raw_input("How many reactions would you like to change?: ")
+                        how_many_input = True
+                        for i in range(int(how_many)):
+                            good_input_1 = False
+                            while good_input_1 == False:
+                                ##############GETTING THE REACTIONS TO CHANGE#################
+                                to_change = raw_input("What reaction should be changed? (enter reactants followed by a '; ' followed by the products, and separate the species within the reactants and products with ', '): ")
+                                reactants_and_products = to_change.split('; ')
+                                kida_tuple = is_in_KIDA(reactants_and_products[0].split(', '), reactants_and_products[1].split(', '), old_vals)
+                                if kida_tuple[0] == True:
+                                    reaction = kida_tuple[1]
+                                    good_input_1 = True
+                                    good_input_2 = False
+                                    while good_input_2 == False:        
+                                        try:
+                                            ##########GETTING THE NEW COEFFICIENTS FOR THE REACTION################        
+                                            new_vals_raw = raw_input("What are the new Arrhenius coefficients for the " + reaction.visualize() + " reaction? (separate coefficients with ', '): ")
+                                            print
+                                            new_vals = new_vals_raw.split(", ")
+                                            fortran_formatted_vals = fortran_format(new_vals)
+                                            change_val(reaction, old_vals, fortran_formatted_vals)
+                                            good_input_2 = True
+                                        except ValueError:
+                                            print "That wasn't a valid number!!!!"
+                                else:
+                                    print "Oops! That reaction is not in KIDA!"
+                                    good_input_1 = False
+                                    
+                    except ValueError:
+                        print "That wasn't a valid number!!!!"
+                        how_many_input = False
+                        
+                write_new_vals(f, old_vals)
+                print "Done! The changes have been saved in " + f
+                ##########ASKING FOR ANY ADDITIONAL CHANGES##############
+                are_you_good =  raw_input("Want to make any more changes to the " + f + " network? (YES or NO): ")
+                if are_you_good == "NO" or are_you_good == "no" or are_you_good == "N" or are_you_good == "n":
+                    print
+                    print "You're good to go!"
+                    good_to_go = True
+                    good_yes_no = True
+        else:
+            print "Whoops! That was a YES or NO question. Try again and don't mess up."
